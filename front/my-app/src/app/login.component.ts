e5e326ac2ee2f86c59ff37abc830499c57361a31
Sasha <sasha361322@gmail.com>
2017-06-21 23:55:49 +0300

    .

--- front/my-app/src/app/login.component.ts
+++ front/my-app/src/app/login.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {AuthService} from "./auth-service";
-import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'my-app',
